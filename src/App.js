afe1c58903c0858be5cdff1652ac9b9f2745d84c
niraj patidar <nirajpatidar11@gmail.com>
2022-02-13 15:54:02 +0530

    #2 add material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-
+import Button from '@mui/material/Button';
+import { Container, createTheme, ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+import { Typography, Link } from '@mui/material';
+import { Box } from '@mui/system';
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box>
+      <Header/>
+        <Container >
+          
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="logo" alt="logo" />
+              <Typography component={'p'}>
+                Edit <code>src/App.js</code> and save to reload.
+              </Typography>
+              <Link
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                color={'secondary'}
+              >
+                Learn React
+              </Link>
+              <Button color="primary" variant="contained">Hello World</Button>
+            </header>
+          </div>
+        </Container>
+      </Box>
+
+
+    </ThemeProvider>
   );
 }
 
